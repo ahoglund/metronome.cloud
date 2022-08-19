5fe8994f575cf4ce58f348f92cc049f9e6351586
Andrew Hoglund <ahoglund@github.com>
2022-08-19 15:55:31 -0500

    time signature

--- js/metronome.js
+++ js/metronome.js
@@ -1,13 +1,14 @@
-var audioContext      = null;
-var isPlaying         = false;
-var tempo             = 60.0;
-var lookahead         = 25.0;
-var currentTick       = 0;
-var scheduleAheadTime = 0.1
-var nextNoteTime      = 0.0;
-var noteLength        = 0.05;
-var worker            = null;
-var woodblock         = null;
+var isPlaying          = false;
+var lookahead          = 25.0;
+var scheduleAheadTime  = 0.1;
+var nextNoteTime       = 0.0;
+var tempo              = null;
+var worker             = null;
+var woodblock          = null;
+var beatsPerBar        = null;
+var beatType           = null;
+var currentBeat        = 0;
+var  currentBeatDisplay = null;
 
 function toggle() {
   isPlaying = !isPlaying;
@@ -17,21 +18,32 @@ function toggle() {
     worker.postMessage("start");
     return "STOP";
   } else {
+    currentBeat = 0;
     worker.postMessage("stop");
     return "START";
   }
 }
 
 function nextNote() {
+  currentBeat++;
   nextNoteTime += 60.0 / tempo;
+
+  if (currentBeat % beatsPerBar == 0) {
+    currentBeat = 0;
+  }
+  currentBeatDisplay.innerHTML = currentBeat;
 }
 
 function scheduleNote(time) {
   var source = audioContext.createBufferSource();
   source.buffer = woodblock;
+  if(currentBeat % beatsPerBar == 0) {
+    source.playbackRate.value = 1.1;
+  } else {
+    source.playbackRate.value = 1;
+  }
   source.connect(audioContext.destination);
   source.start(time);
-  source.stop(time + noteLength);
 }
 
 function scheduler() {
@@ -60,15 +72,31 @@ function init(){
 
   loadSound("sounds/woodblock.ogg");
 
+  var toggleButton = document.getElementById("toggle-button");
+  toggleButton.addEventListener("click", function() {
+    this.innerHTML = toggle();
+  });
 
-  var tempoSlider = document.getElementById("tempo-slider");
-  var tempoDisplay = document.getElementById("tempo-display");
+  var tempoDisplay       = document.getElementById("tempo-display");
+  var tempoSlider        = document.getElementById("tempo-slider");
+  tempo = tempoSlider.value;
   tempoDisplay.innerHTML = tempoSlider.value;
 
   tempoSlider.oninput = function() {
-    tempo             = this.value;
-    tempoDisplay.innerHTML  = this.value;
+    tempo = this.value;
+    tempoDisplay.innerHTML = this.value;
   }
+
+  var beatPerBarDisplay  = document.getElementById("beat-per-bar-display");
+  var beatTypeDisplay    = document.getElementById("beat-type-display");
+  currentBeatDisplay = document.getElementById("current-beat");
+
+  beatPerBarDisplay.oninput = function() {
+    beatsPerBar = this.value;
+  }
+
+  beatsPerBar = beatPerBarDisplay.value;
+  beatType    = beatTypeDisplay.value;
 
   worker.onmessage = function(e) {
     if (e.data == "tick")
